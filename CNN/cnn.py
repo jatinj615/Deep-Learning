0f4eec240b784af4a070f276cacbe2fd7076a5fd
Jatin <jatinj615@gmail.com>
2018-05-07 23:25:13 +0530

    boltzman_machine

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -5,57 +5,62 @@ from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
-# Initialising CNN
-clf = Sequential()
-
-# Step1 - convolution layer
-
-clf.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3 ), activation= 'relu'))
-
-# Step2 - Max Pooling
-clf.add(MaxPooling2D(pool_size= (2, 2)))
-
-#Adding another convolutional layer
-clf.add(Convolution2D(32, (3, 3), activation= 'relu'))
-clf.add(MaxPooling2D(pool_size= (2, 2)))
-
-#Step3 - Flattening
-clf.add(Flatten())
-
-# Step4 - Full connection
-clf.add(Dense(output_dim = 128, activation = 'relu'))
-clf.add(Dense(output_dim = 1, activation = 'sigmoid'))
-
-# Step5 compile CNN
-clf.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Fitting CNN to images
-from keras.preprocessing.image import ImageDataGenerator
-
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+with tf.device('/CPU:0'):
+    from keras.preprocessing.image import ImageDataGenerator
+    
+    train_datagen = ImageDataGenerator(rescale=1./255,
+                                       shear_range=0.2,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True)
+                                
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    
+    training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                     target_size=(64, 64),
+                                                     batch_size=32,
+                                                     class_mode='binary')
+    
+    test_set = test_datagen.flow_from_directory('dataset/test_set',
                                                  target_size=(64, 64),
                                                  batch_size=32,
                                                  class_mode='binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                             target_size=(64, 64),
-                                             batch_size=32,
-                                             class_mode='binary')
-with tf.device('/gpu:0'):
-    clf.fit_generator(training_set,
-                        steps_per_epoch=8000,
-                        epochs=25,
-                        validation_data=test_set,
-                        validation_steps=2000)
+
+with tf.device('/GPU:0'):
+
+    # Initialising CNN
+    clf = Sequential()
     
+    # Step1 - convolution layer
+    
+    clf.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3 ), activation= 'relu'))
+    
+    # Step2 - Max Pooling
+    clf.add(MaxPooling2D(pool_size= (2, 2)))
+    
+    #Adding another convolutional layer
+    clf.add(Convolution2D(32, (3, 3), activation= 'relu'))
+    clf.add(MaxPooling2D(pool_size= (2, 2)))
+    
+    #Step3 - Flattening
+    clf.add(Flatten())
+    
+    # Step4 - Full connection
+    clf.add(Dense(output_dim = 128, activation = 'relu'))
+    
+    # Adding output layer
+    clf.add(Dense(output_dim = 1, activation = 'sigmoid'))
+    
+    # Step5 compile CNN
+    clf.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    # Fitting CNN to images
+    clf.fit_generator(training_set,
+                      steps_per_epoch=8000,
+                      epochs=25,
+                      validation_data=test_set,
+                      validation_steps=2000)
+
 
 from keras.models import model_from_json
 model_json = clf.to_json()
