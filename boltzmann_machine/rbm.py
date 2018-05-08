8053f3d77a97ce616184ad3ac162792b74db0312
Jatin <jatinj615@gmail.com>
2018-05-08 22:53:49 +0530

    data preprocessing

--- boltzmann_machine/rbm.py
+++ boltzmann_machine/rbm.py
@@ -14,6 +14,65 @@ movies = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python',
 users =  pd.read_csv('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
 ratings =  pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python', encoding='latin-1')
 
+# Preparing training set and test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype = 'int')
+
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
+
+# Getting number of users and movies
+nb_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
+nb_movies = int(max(max(training_set[:, 1]), max(test_set[:, 1])))
+
+# converting data into array where users are rows and movies are columns
+def convert(data):
+    new_data = []
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:, 1][data[:, 0] == id_users]
+        id_ratings = data[:, 2][data[:, 0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+
+training_set = convert(training_set)
+test_set = convert(test_set)
+    
+# Converting the data into Torch tensors
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
+
+# Converting ratings to binary
+training_set[training_set == 0] = -1
+training_set[training_set <= 2] = 0
+training_set[training_set >= 3] = 1
+test_set[test_set == 0] = -1
+test_set[test_set <= 2] = 0
+test_set[test_set >= 3] = 1
+
+# creating the architecture of the neural network
+class RBM():
+    def __init__(self, nv, nh):
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
